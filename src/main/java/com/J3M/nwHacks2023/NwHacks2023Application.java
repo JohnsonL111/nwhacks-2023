6979d02f1902b9f945cb477f2ff5d3a4c53e87f6
Joshua Li <JoshuaJLi@outlook.com>
2023-01-21 14:14:16 -0800

    Initial commit.

---
+++ src/main/java/com/J3M/nwHacks2023/NwHacks2023Application.java
@@ -0,0 +1,13 @@
+package com.J3M.nwHacks2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwHacks2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwHacks2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/J3M/nwHacks2023/NwHacks2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.J3M.nwHacks2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwHacks2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
