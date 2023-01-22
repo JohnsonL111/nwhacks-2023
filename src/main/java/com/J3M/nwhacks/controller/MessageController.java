206a7f598f38508f23bf0465bc885e50d3969863
Johnson Luong <jkl53@sfu.ca>
2023-01-21 20:12:32 -0800

    Add basic twillio messaging test

--- src/main/java/com/J3M/nwhacks/controller/MessageController.java
+++ src/main/java/com/J3M/nwhacks/controller/MessageController.java
@@ -1,18 +1,41 @@
 package com.J3M.nwhacks.controller;
 
-import com.J3M.nwhacks.repository.LocationRepository;
+import com.J3M.nwhacks.service.MessagingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
-    private final LocationRepository locationRepository;
+    // dependency injection via requiredArgsConstructor @ param
+    private final MessagingService messageService;
+    public static final String ACCOUNT_SID = "AC254c116cf838956a559ddce050c844cc";
+    public static final String AUTH_TOKEN = "7e8cf58fce530662f7a932690aa3234a";
 
     @GetMapping("/message")
     public String sendMessage() {
-        return "msg";
+        // scrape all numbers
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        System.out.println(ACCOUNT_SID);
+        System.out.println(AUTH_TOKEN);
+
+        Message message = Message.creator(
+                        new com.twilio.type.PhoneNumber("+16043792278"),
+                        new com.twilio.type.PhoneNumber("+16088796642"),
+                        "Hi there")
+                .create();
+
+        return message.getSid();
+
+
+        // List<String> contactPeopleNumbers, String personOfInterest, String location
+        // messageService.messagePeople();
     }
 
 
