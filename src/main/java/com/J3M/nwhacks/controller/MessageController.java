4fd15fbe56d76eea3b9c8ba02d8c29d203e601f1
Johnson Luong <jkl53@sfu.ca>
2023-01-21 21:39:23 -0800

    Add ping all users functionality

--- src/main/java/com/J3M/nwhacks/controller/MessageController.java
+++ src/main/java/com/J3M/nwhacks/controller/MessageController.java
@@ -4,9 +4,8 @@ import com.J3M.nwhacks.service.MessagingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+
+import java.util.List;
 
 
 @RestController
@@ -17,27 +16,12 @@ public class MessageController {
     public static final String ACCOUNT_SID = "AC254c116cf838956a559ddce050c844cc";
     public static final String AUTH_TOKEN = "7e8cf58fce530662f7a932690aa3234a";
 
+    // testing endpoint
     @GetMapping("/message")
-    public String sendMessage() {
-        // scrape all numbers
-
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        System.out.println(ACCOUNT_SID);
-        System.out.println(AUTH_TOKEN);
-
-        Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber("+16043792278"),
-                        new com.twilio.type.PhoneNumber("+16088796642"),
-                        "Hi there")
-                .create();
-
-        return message.getSid();
-
-
+    public void sendMessage() {
+        // testing endpoint
         // List<String> contactPeopleNumbers, String personOfInterest, String location
-        // messageService.messagePeople();
+        // person of interest moved to location
+        messageService.messagePeople(List.of("+16043792278", "+16044423403"), "bob", "vancouver");
     }
-
-
-
 }
