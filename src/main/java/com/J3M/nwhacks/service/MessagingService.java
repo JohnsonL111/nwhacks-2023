c3ca4266375d5c1d9ce76d8fa0bd66b1e80438ea
Joshua Li <JoshuaJLi@outlook.com>
2023-01-21 17:57:59 -0800

    Stub class for messaging.

---
+++ src/main/java/com/J3M/nwhacks/service/MessagingService.java
@@ -0,0 +1,18 @@
+package com.J3M.nwhacks.service;
+
+import com.J3M.nwhacks.model.Location;
+import com.J3M.nwhacks.model.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class MessagingService {
+
+    void messagePeople(List<String> contactPeopleNumbers, String personOfInterest, String location) {
+
+    }
+}
